25466546f00e661574387ca7ad7f5bd1c767a323
Thomas WILLEM <Thomas.WILLEM@akkodis.com>
2025-07-22 09:45:37 +0200

    Correction import launch inference

--- code/inference/launch_inference.py
+++ code/inference/launch_inference.py
@@ -14,7 +14,7 @@ from tools import menu as sl
 from tools import error_report as re
 from tools import various_functions as vf
 
-from inference.Inference import Inference
+from inference.inference import Inference
 
 #===========================================================================
 class LaunchInference:
