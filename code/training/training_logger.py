af0950f771ef881214e5d09cc7fd93fe6e7168e3
Pierre-Fa <p.fancelli@akkodis.com>
2025-10-28 14:50:29 +0100

    update

--- code/training/training_logger.py
+++ code/training/training_logger.py
@@ -189,7 +189,7 @@ class TrainingLogger:
         ax0.set_xlabel('Epochs')
         ax0.set_ylabel('Loss Value')
         ax0.legend()
-        ax0.xaxis.set_major_locator(MaxNLocator(integer=True))  # labels entiers sur x
+        ax0.xaxis.set_major_locator(MaxNLocator(integer=True))
 
         ax1 = axes[1]
         for metric in metrics_dict['train']:
@@ -202,7 +202,7 @@ class TrainingLogger:
         ax1.set_xlabel('Epochs')
         ax1.set_ylabel('Metric Values')
         ax1.legend()
-        ax1.xaxis.set_major_locator(MaxNLocator(integer=True))  # labels entiers sur x
+        ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
 
         plt.tight_layout()
         plt.savefig(os.path.join(self.save_directory, 'learning_curves.png'), dpi=300)
