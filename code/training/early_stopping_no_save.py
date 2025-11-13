b50d872ed264d25d87687eeb75356512bfd68529
Zhijian ZHOU <zhijian2040.zhou@gmail.com>
2025-11-13 18:01:13 +0100

    early_stopping_no_save.py

--- code/training/early_stopping_no_save.py
+++ code/training/early_stopping_no_save.py
@@ -1,5 +1,5 @@
 from early_stopping_pytorch import EarlyStopping
-
+"""Z: used by training.py"""
 class EarlyStoppingNoSave(EarlyStopping):
     def save_checkpoint(self, val_loss, model):
         # On override la fonction pour ne rien faire
